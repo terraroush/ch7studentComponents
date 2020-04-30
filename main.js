24954043724dfd59b4778453a4166fcc79a045c3
Terra Roush <terra.roush@gmail.com>
2020-04-29 20:58:01 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,116 @@
+const students = [
+    {
+        name: "Chris Miller",
+        subject: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        subject: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        subject: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        subject: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        subject: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        subject: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        subject: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        subject: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        subject: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        subject: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        subject: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        subject: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+const createStudentComponent = (name, subject, info, status) => {
+    return `
+        <div class="student">
+            <h1 class="${status}">${name}</h1>
+            <section>${subject}</section>
+            <aside>${info}</aside>
+        </div>
+    `
+}
+
+const studentContainer = document.querySelector("#container")
+// for (let i = 0; i < students.length; i++) {
+//     const student = students[i];
+
+//     let newStudent = createStudentComponent(
+//         student.name,
+//         student.subject,
+//         student.info
+//     )
+//   studentContainer.innerHTML += newStudent  
+// }
+
+for (const student of students) {
+    let studentComponent = ""
+    if (student.score >= 60) {
+        studentComponent = createStudentComponent(
+            student.name,
+            student.subject,
+            student.info,
+            "passing"
+        )
+    } else {
+        studentComponent = createStudentComponent(
+            student.name,
+            student.subject,
+            student.info,
+            "failing"
+        )
+    }
+    studentContainer.innerHTML += studentComponent 
+}
